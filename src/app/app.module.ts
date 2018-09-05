86ce0820ec6d0cb33a638b31f8e27090c37f87d3
Estrada Navarro Almendra Lucia <aestrada@americatel.com.pe>
2018-09-04 19:42:04 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { Body_mineComponent } from './components/body_mine/body_mine.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    Body_mineComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/body_mine/body_mine.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:"app-body_mine",
+    templateUrl:"./body_mine.component.html"
+})
+export class Body_mineComponent{
+
+}
